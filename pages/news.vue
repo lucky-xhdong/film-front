1059924043b277dd0476c46c43ea26f0d43667ac
meeting_film@sina.com <meeting_film@sina.com>
2019-12-27 23:53:27 +0800

    仿猫眼电影网站

---
+++ pages/news.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div>
+      <!--<Container/>-->
+    </div>
+  </div>
+</template>
+<script>
+  // import Container from '~/components/index/Container.vue'
+  // import Banner from '~/components/index/Banner.vue'
+  //
+  //
+  // export default {
+  //   components: {
+  //     Container,
+  //   }
+  // }
+
+</script>
+
+<style lang="scss" scoped>
+</style>
